5c2d5a2e4a6bc38cdc7fe9f02cf6e274df2b05b7
Ramblec <86812768+Ramblec@users.noreply.github.com>
2024-01-05 13:29:16 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+  console.log("Hello, World!");
+})
+
+server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`))
